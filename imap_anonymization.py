40be30865f793f3f7e64820458809aeeddb95bbc
Arne Lieten <arne.lieten@hotmail.com>
2025-08-02 20:31:48 +0200

    completed the mailbox_to_json function and added the mailbox.json to the gitignore

--- imap_anonymization.py
+++ imap_anonymization.py
@@ -1,28 +1,31 @@
-from imap_tools import MailBox
+from imap_tools import MailBox, AND
 from dotenv import dotenv_values
+from datetime import date
+import json
 
 
 def flatten_email(text):
     return text.replace('\r', '').replace('\n', ' ')
 
-def anonymize_email(msg):
+def anonymize_email(email):
     print("not ready yet")
 
-def imap_to_json():
+def mailbox_to_json():
     config = dotenv_values(".env")
-    MAIL_USERNAME= config["MAIL_USERNAME"]
-    MAIL_PASSWORD= config["MAIL_PASSWORD"]
+    MAIL_USERNAME = config["MAIL_USERNAME"]
+    MAIL_PASSWORD = config["MAIL_PASSWORD"]
+    CUTOFF_DATE = date(2024, 8, 1) # check this out tomorrow if it works!!
+    OUTPUT_PATH = "C:\\Users\\arnel\\Projects\\anonymization\\mailbox.json"
+    records = []
 
     with MailBox("imap.gmail.com").login(MAIL_USERNAME, MAIL_PASSWORD, "[Gmail]/Sent Mail") as mb:
-        for msg in mb.fetch(limit=1, reverse=True, mark_seen=False):
-            record = {
-                "subject" : msg.subject,
-                "sender" : msg.from_,
-                "receiver" : msg.to,
-                "date" : msg.date,
-                "content" : flatten_email(msg.text)
-            }
+        for email in mb.fetch(AND(date_gte=CUTOFF_DATE), reverse=True, mark_seen=False):
+            records.append ({
+                "subject" : email.subject,
+                "body" : flatten_email(email.text)
+            })
 
-            print(record)
+    with open(OUTPUT_PATH, 'w', encoding='utf-8') as f:
+        json.dump(records, f, indent=2)
 
-imap_to_json()
+mailbox_to_json()
