4e4cb7181bd1b6fc1e1b5229e81502b3136ab26a
Arne Lieten <arne.lieten@hotmail.com>
2025-08-03 11:00:55 +0200

    implemented anonymization function based on presidio, SpaCy and nl_core_news_lg

--- imap_anonymization.py
+++ imap_anonymization.py
@@ -1,20 +1,43 @@
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
+from presidio_analyzer.nlp_engine import NlpEngineProvider
 from imap_tools import MailBox, AND
 from dotenv import dotenv_values
 from datetime import date
 import json
 
+def setup_analyzer():
+    configuration = {
+        "nlp_engine_name": "spacy",
+        "models": [{"lang_code": "nl", "model_name": "nl_core_news_lg"}],
+    }
+
+    provider = NlpEngineProvider(nlp_configuration=configuration)
+    nlp_engine = provider.create_engine()
+
+    analyzer = AnalyzerEngine(nlp_engine=nlp_engine, supported_languages=["nl"])
+    return analyzer
+
+
+def anonymize_email(text):
+    analyzer = setup_analyzer()
+    anonymizer = AnonymizerEngine()
+
+    results = analyzer.analyze(text=text, language="nl")
+    anonymized_result = anonymizer.anonymize(text=text, analyzer_results=results)
+
+    return anonymized_result.text
+
 
 def flatten_email(text):
     return text.replace('\r', '').replace('\n', ' ')
 
-def anonymize_email(email):
-    print("not ready yet")
 
 def mailbox_to_json():
     config = dotenv_values(".env")
     MAIL_USERNAME = config["MAIL_USERNAME"]
     MAIL_PASSWORD = config["MAIL_PASSWORD"]
-    CUTOFF_DATE = date(2024, 8, 1) # check this out tomorrow if it works!!
+    CUTOFF_DATE = date(2025, 8, 2)
     OUTPUT_PATH = "C:\\Users\\arnel\\Projects\\anonymization\\mailbox.json"
     records = []
 
@@ -22,7 +45,7 @@ def mailbox_to_json():
         for email in mb.fetch(AND(date_gte=CUTOFF_DATE), reverse=True, mark_seen=False):
             records.append ({
                 "subject" : email.subject,
-                "body" : flatten_email(email.text)
+                "body" : anonymize_email(flatten_email(email.text))
             })
 
     with open(OUTPUT_PATH, 'w', encoding='utf-8') as f:
